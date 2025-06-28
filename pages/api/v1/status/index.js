772b06583c57e779469d10c7660bfd1dfcaea5d4
GuilhermeLangkammer <65038548+GuilhermeLangkammer@users.noreply.github.com>
2025-06-28 23:41:51 +0000

    criação do modulo `database` e criação do arquivo `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,9 @@
-function status(request, response) {
-  response.status(200).json({
-    chave: "deu bom",
-  });
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 2 + 1 as sum;");
+  console.log(result);
+  response.status(200).json({ chave: "deu bom" });
 }
 
 export default status;
